f302a6d916bd8eb6ebb4a4e58705c32d01ead7f8
Hsieh Chin Fan <typebrook@gmail.com>
2020-10-01 13:37:35 +0800

    Add Mapbox Map

---
+++ app/src/main/java/com/example/sample/MapboxFragment.kt
@@ -0,0 +1,34 @@
+package com.example.sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+
+class MapboxFragment : Fragment(), OnMapReadyCallback {
+
+    private val mapView by lazy {
+        Mapbox.getInstance(requireContext(), null)
+        MapView(requireContext()).apply { getMapAsync(this@MapboxFragment) }
+    }
+    private val defaultStyle by lazy { requireContext().getString(R.string.uri_style_rudymap) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View = mapView
+
+    override fun onMapReady(mapboxMap: MapboxMap) = with(mapboxMap) {
+        setStyle(defaultStyle)
+        cameraPosition = CameraPosition.Builder()
+            .target(LatLng(25.023167, 121.585674))
+            .zoom(12.0)
+            .build()
+    }
+}
--- app/src/main/java/com/example/sample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/sample/ui/main/MainFragment.kt
@@ -6,26 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.sample.MapboxFragment
 import com.example.sample.R
-import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
     private val viewModel by activityViewModels<MainViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        button.setOnClickListener {
-
+        requireActivity().supportFragmentManager.commit {
+            add<MapboxFragment>(R.id.map_container, null)
         }
+
+        return inflater.inflate(R.layout.main_fragment, container, false)
     }
 }
