d4b949a13fe559a461660065aa3047170fd0204b
Hsieh Chin Fan <typebrook@gmail.com>
2020-10-14 18:17:07 +0800

    Refactor CoordRefSys

--- app/src/main/java/com/example/sample/geometry/CoordRefSys.kt
+++ app/src/main/java/com/example/sample/geometry/CoordRefSys.kt
@@ -16,21 +16,18 @@ val isValidInWGS84: (XYPair) -> Boolean = { (lon, lat) ->
     lat > -90 && lat < 90 && lon > -180 && lon < 180
 }
 
-fun XYPair.convert(from: CoordRefSys, to: CoordRefSys) = CoordRefSys.generateConverter(from, to)(this)
+fun XYPair.convert(from: CoordRefSys, to: CoordRefSys) = from.converterFor(to)(this)
 fun XYPair.isValid(crs: CoordRefSys) = isValidInWGS84(convert(crs, CoordRefSys.WGS84))
 
-enum class ParameterType { Code, Proj4 }
-enum class CoordExpression { SINGLE, XY, Degree, DegMin, DMS }
+enum class CoordExpression { Degree, DegMin, DMS, SINGLE, XY }
+val selectableExpressions = CoordExpression.values().toList().subList(0, 3)
 
 open class CoordRefSys(
-        val displayName: String = "UnNamed",
-        private val type: ParameterType = ParameterType.Code,
-        val parameter: String
+    val displayName: String = "UnNamed",
+    private val type: ParameterType = ParameterType.Code,
+    val parameter: String
 ) {
-    val isLonLat: Boolean by lazy {
-        val converter = generateConverter(WGS84, this)
-        converter(179.0 to 89.0).let { (x, y) -> x < 180 && y < 90 }
-    }
+    enum class ParameterType { Code, Proj4 }
 
     val crs: CoordinateReferenceSystem by lazy {
         @Throws(UnknownAuthorityCodeException::class)
@@ -40,24 +37,53 @@ open class CoordRefSys(
         }
     }
 
+    val isLongLat: Boolean
+        get() = crs.parameterString.startsWith("+proj=longlat")
+
+    fun converterFor(crs2: CoordRefSys): CoordConverter {
+        if (this == crs2) return { xyPair -> xyPair }
+
+        val trans = CoordinateTransformFactory().createTransform(crs, crs2.crs)
+        return { (x, y): XYPair ->
+            val p1 = ProjCoordinate(x, y)
+            val p2 = ProjCoordinate()
+            trans.transform(p1, p2)
+
+            p2.x to p2.y
+        }
+    }
+
+    override fun equals(other: Any?): Boolean =
+        (other is CoordRefSys && parameter == other.parameter)
+
+    override fun hashCode(): Int {
+        var result = displayName.hashCode()
+        result = 31 * result + type.hashCode()
+        result = 31 * result + parameter.hashCode()
+        return result
+    }
+
     companion object {
 
-        fun generateConverter(crs1: CoordRefSys, crs2: CoordRefSys = WGS84): CoordConverter {
-            if (crs1.parameter == crs2.parameter) return { xyPair -> xyPair }
-
-            val trans = CoordinateTransformFactory().createTransform(crs1.crs, crs2.crs)
-            return { (x, y): XYPair ->
-                val p1 = ProjCoordinate(x, y)
-                val p2 = ProjCoordinate()
-                trans.transform(p1, p2)
-
-                p2.x to p2.y
-            }
-        }
-
-        val WGS84 = CoordRefSys("WGS84", ParameterType.Code, "EPSG:4326")
-        val TWD97 = CoordRefSys("TWD97", ParameterType.Code, "EPSG:3826")
-        val TWD67 = CoordRefSys("TWD67", ParameterType.Proj4, "+proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=aust_SA  +towgs84=-750.739,-359.515,-180.510,0.00003863,0.00001721,0.00000197,0.99998180 +units=m +no_defs")
-        val TWD67_latLng = CoordRefSys("TWD67(經緯度)", ParameterType.Proj4, "+proj=longlat +ellps=aust_SA  +towgs84=-750.739,-359.515,-180.510,0.00003863,0.00001721,0.00000197,0.99998180 +no_defs")
+        val WGS84 = CoordRefSys(
+            displayName = "WGS84",
+            type = ParameterType.Code,
+            parameter = "EPSG:4326"
+        )
+        val TWD97 = CoordRefSys(
+            displayName = "TWD97",
+            type = ParameterType.Code,
+            parameter = "EPSG:3826"
+        )
+        val TWD67 = CoordRefSys(
+            displayName = "TWD67",
+            type = ParameterType.Proj4,
+            parameter = "+proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=aust_SA  +towgs84=-750.739,-359.515,-180.510,0.00003863,0.00001721,0.00000197,0.99998180 +units=m +no_defs"
+        )
+        val TWD67_latLng = CoordRefSys(
+            displayName = "TWD67(經緯度)",
+            type = ParameterType.Proj4,
+            parameter = "+proj=longlat +ellps=aust_SA  +towgs84=-750.739,-359.515,-180.510,0.00003863,0.00001721,0.00000197,0.99998180 +no_defs"
+        )
     }
 }
