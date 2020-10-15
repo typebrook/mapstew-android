f960f406395c38f1f8f82d071d1b4ad90a774696
Hsieh Chin Fan <typebrook@gmail.com>
2020-10-15 16:54:49 +0800

    Improve MapboxFragment with native SupportMapFragment

--- app/src/main/java/com/example/sample/map/MapboxFragment.kt
+++ app/src/main/java/com/example/sample/map/MapboxFragment.kt
@@ -1,10 +1,6 @@
 package com.example.sample.map
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.content.Context
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.observe
 import com.example.sample.R
@@ -12,23 +8,18 @@ import com.example.sample.ui.main.MapViewModel
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.geometry.LatLng
-import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.SupportMapFragment
 
-class MapboxFragment : Fragment(), OnMapReadyCallback {
+class MapboxFragment : SupportMapFragment() {
 
     private val model by activityViewModels<MapViewModel>()
-
-    private val mapView by lazy {
-        Mapbox.getInstance(requireContext(), null)
-        MapView(requireContext()).apply { getMapAsync(this@MapboxFragment) }
-    }
     private val defaultStyle by lazy { requireContext().getString(R.string.uri_style_rudymap) }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View = mapView
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Mapbox.getInstance(requireContext(), null)
+    }
 
     override fun onMapReady(mapboxMap: MapboxMap) = with(mapboxMap) {
 
