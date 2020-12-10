9ff11db4d299a64290a747f500a1e661f5349eeb
Hsieh Chin Fan <typebrook@gmail.com>
2020-12-10 22:30:02 +0800

    Improve UI for map download
    - Add Material theme and simpler style setting
    - Click to download coresponding MBTiles

--- app/src/main/java/com/example/sample/map/OfflineFragment.kt
+++ app/src/main/java/com/example/sample/map/OfflineFragment.kt
@@ -9,41 +9,59 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import com.example.sample.R
 import com.example.sample.databinding.OfflineMapItemBinding
 import com.example.sample.network.DownloadWorker
+import com.google.android.material.button.MaterialButton
 
 
 class OfflineFragment : DialogFragment() {
 
-    data class OfflineMap(val displayName: String, val path: String)
+    inner class OfflineMap(val displayName: String, val path: String) {
+        val isValid = requireContext().getDatabasePath(path.substringAfterLast("/")).exists()
+    }
 
-    val maps = listOf(
-        OfflineMap("Contours", "typebrook/contours/releases/download/2020/contours.mbtiles"),
-        OfflineMap("Rudymap", "typebrook/mapstew/releases/download/daily-taiwan-pbf/taiwan-daily.osm.pbf")
-    )
+    val maps by lazy {
+        listOf(
+            OfflineMap(
+                displayName = "Contours",
+                path = "typebrook/contours/releases/download/2020/contours.mbtiles"
+            ),
+            OfflineMap(
+                displayName = "Rudymap",
+                path = "typebrook/mapstew/releases/download/daily-taiwan-pbf/taiwan-daily.osm.pbf"
+            )
+        )
+    }
 
-    private val adapter = object : BaseAdapter() {
+    private val adapter by lazy {
+        object : BaseAdapter() {
+            @SuppressLint("ViewHolder")
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                val map = getItem(position)
+                val layoutInflater =
+                    requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                val binding = OfflineMapItemBinding.inflate(layoutInflater)
 
-        @SuppressLint("ViewHolder")
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val map = getItem(position)
-            val layoutInflater =
-                requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val binding = OfflineMapItemBinding.inflate(layoutInflater)
+                binding.name.text = map.displayName
 
-            binding.name.text = map.displayName
-            with(binding.button) {
-                text = "Download"
-                setOnClickListener { DownloadWorker.enqueue(requireContext(), map.path) }
-
+                with(binding.button) {
+                    text = if (map.isValid) getString(R.string.in_use) else "Download"
+                    setOnClickListener { DownloadWorker.enqueue(requireContext(), map.path) }
+                    if (map.isValid) {
+                        isClickable = false
+                        setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+                    }
+                }
+                return binding.root
             }
-            return binding.root
-        }
 
-        override fun getCount(): Int = maps.size
-        override fun getItem(position: Int): OfflineMap = maps[position]
-        override fun getItemId(position: Int): Long = getItem(position).hashCode().toLong()
+            override fun getCount(): Int = maps.size
+            override fun getItem(position: Int): OfflineMap = maps[position]
+            override fun getItemId(position: Int): Long = getItem(position).hashCode().toLong()
+        }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
