56d310d6ba201f708264f5c6b6f20bb051420c42
Hsieh Chin Fan <typebrook@gmail.com>
2021-01-21 22:55:56 +0800

    Use style.xml to make APP Bar translucent

--- app/src/main/java/settings/SettingsFragment.kt
+++ app/src/main/java/settings/SettingsFragment.kt
@@ -1,12 +1,24 @@
 package settings
 
 import android.os.Bundle
+import android.view.View
 import androidx.preference.PreferenceFragmentCompat
 import io.typebrook.mapstew.R
+
 
 // TODO add real settings for options
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.setPadding(
+            view.paddingStart,
+            200,
+            view.paddingEnd,
+            view.paddingBottom
+        )
     }
 }
