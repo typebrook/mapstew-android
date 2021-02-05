3a0786b29aeb7ed353f5d86512b3ea5a53c405ca
Hsieh Chin Fan <typebrook@gmail.com>
2021-02-06 01:07:06 +0800

    Modify logic about SafeMutableLiveData
    1. By default, do not set value if new value is the same
    2. Override predicate to change behavior above
    3. Use 'initValue' in constructor for less confusing
    4. Reformat code

--- app/src/main/java/io/typebrook/mapstew/livedata/SafeMutableLiveData.kt
+++ app/src/main/java/io/typebrook/mapstew/livedata/SafeMutableLiveData.kt
@@ -3,17 +3,19 @@ package io.typebrook.mapstew.livedata
 import androidx.lifecycle.LiveData
 
 @Suppress("UNCHECKED_CAST")
-open class SafeMutableLiveData<T>(value: T) : LiveData<T>(value) {
+open class SafeMutableLiveData<T>(initValue: T) : LiveData<T>(initValue) {
 
     override fun getValue(): T = super.getValue() as T
 
-    public override fun setValue(value: T) =
-        if (predicate(value) && value != this.value) super.setValue(transformer(value)) else Unit
+    public override fun setValue(value: T) = if (predicate(value))
+        super.setValue(transformer(value)) else
+        Unit
 
-    public override fun postValue(value: T) =
-        if (predicate(value) && value != this.value) super.postValue(transformer(value)) else Unit
+    public override fun postValue(value: T) = if (predicate(value))
+        super.postValue(transformer(value)) else
+        Unit
 
-    protected open val predicate: (T) -> Boolean = { true }
+    protected open val predicate: (T) -> Boolean = { newValue -> newValue != value }
     protected open val transformer: (T) -> T = { new -> new }
 }
 
